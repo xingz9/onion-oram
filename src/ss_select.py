8cee944ce394feb47005779917d99c10c27893aa
Xing Zhou <xingz@fb.com>
2020-07-06 13:33:42 -0400

    Add ServerPolyData and ClientPolyData

--- src/ss_select.py
+++ src/ss_select.py
@@ -1,13 +1,16 @@
 import random
 
 
+WORD_BITS = 64
+
+
 class RandomGenerator:
 
     def __init__(self):
         pass
 
-    def rand_long(self):
-        return random.getrandbits(64)
+    def rand_long(self, bits=WORD_BITS):
+        return random.getrandbits(bits)
 
 
 class BeaverTriple:
@@ -116,6 +119,54 @@ class ClientData:
         return result
 
 
+class ServerPolyData:
+
+    def __init__(self, x_vec):
+        self.sd = [ServerData(x) for x in x_vec]
+        self.xor = x_vec[0]
+        for i in range(1, len(x_vec)):
+            self.xor ^= x_vec[i]
+
+    def step2(self, comm_list):
+        # Check comm_list.size() == sd.size()
+        result = long(0)
+        for i in range(len(self.sd)):
+            result ^= self.sd[i].step2(comm_list[i])
+        return result
+
+
+class PolyStepResult:
+
+    def __init__(self):
+        self.comm_list = []
+        self.u = long(0)
+        self.v = long(0)
+
+
+class ClientPolyData:
+
+    def __init__(self, y_vec):
+        self.cd = [ClientData(y) for y in y_vec]
+
+    def step1(self, beaver_gen):
+        result = PolyStepResult()
+
+        triple = beaver_gen.gen_beaver_2_share()
+        b2 = triple[1].b
+        step_result = self.cd[0].step1(triple)
+        result.comm_list.append(step_result.comm)
+        result.u = step_result.u
+        result.v = step_result.v
+
+        for i in range(1, len(self.cd)):
+            step_result = self.cd[i].step1(beaver_gen.gen_beaver_2_share(b2))
+            result.comm_list.append(step_result.comm)
+            # Check result.u == step_result.u
+            result.v ^= step_result.v
+
+        return result
+
+
 if __name__ == '__main__':
     gen = RandomGenerator()
     beaver_gen = BeaverGenerator(gen)
@@ -123,8 +174,8 @@ if __name__ == '__main__':
     for _ in range(10):
         x = gen.rand_long()
         y = gen.rand_long()
-        print("x  = 0x{:02X}".format(x))
-        print("y  = 0x{:02X}".format(y))
+        print("x  = 0x{:02x}".format(x))
+        print("y  = 0x{:02x}".format(y))
 
         sd = ServerData(x)
         cd = ClientData(y)
@@ -132,9 +183,23 @@ if __name__ == '__main__':
         step_result = cd.step1(beaver_gen.gen_beaver_2_share())
         z1 = sd.step2(step_result.comm)
         z2 = (x & step_result.u) ^ step_result.v
-        print("z1 = 0x{:02X}".format(z1))
-        print("z2 = 0x{:02X}".format(z2))
-        print("z1 ^ z2 = 0x{:02X}".format(z1 ^ z2))
-        print("x  ^  y = 0x{:02X}".format(x & y))
+        print("z1 = 0x{:02x}".format(z1))
+        print("z2 = 0x{:02x}".format(z2))
+        print("z1 ^ z2 = 0x{:02x}".format(z1 ^ z2))
+        print("x  ^  y = 0x{:02x}".format(x & y))
         print
 
+        x_vec = [i + 1 for i in range(5)]
+        spd = ServerPolyData(x_vec)
+        for select_index in range(len(x_vec)):
+            y_vec = [long(0) for _ in range(len(x_vec))]
+            y_vec[select_index] = (1 << WORD_BITS) - 1
+            cpd = ClientPolyData(y_vec)
+            step_result = cpd.step1(beaver_gen)
+
+            z1 = spd.step2(step_result.comm_list)
+            z2 = (spd.xor & step_result.u) ^ step_result.v
+            print("z1 = 0x{:02x}".format(z1))
+            print("z2 = 0x{:02x}".format(z2))
+            print("z1 ^ z2 = 0x{:02x}".format(z1 ^ z2))
+            print("x[{}] = 0x{:02x}".format(select_index, x_vec[select_index]))
